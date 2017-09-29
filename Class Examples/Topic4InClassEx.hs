df8751911caa0ec969f7ce3ab25b9bba3d489b99
shanesims <shane.sims.ss@gmail.com>
2017-09-29 15:05:11 -0600

    more class examples and ch.5 exercises complete

--- Class Examples/Topic4InClassEx.hs
+++ Class Examples/Topic4InClassEx.hs
@@ -9,7 +9,7 @@ dot :: (Double, Double, Double) -> (Double, Double, Double) -> Double
 dot (x1, y1, z1) (x2, y2, z2) = x1*x2 + y1*y2 + z1*z2
 
 -- Compute length of a vector
-len :: (Double, Double, Double) - > Double
+len :: (Double, Double, Double) -> Double
 len (x,y,z) = sqrt(x*x + y*y + z*z)
 
 -- Example 4: function returns True if list is empty and false otherwise
@@ -30,13 +30,42 @@ isSingletonList _       = False
 
 -- ex 6: function to sum all elements of a list, using pattern matching
 mySum :: [Int] -> Int
-mySum []  = 0
+mySum []  = 0 -- base case: empty list
 -- mySum values = head values + (mySum (tail values))
-mySum (v:vs) = v + mySum vs -- head of list in v and tail in vs;
+mySum (v:vs) = v + mySum vs -- take the from ele of list, name it v, take the remaining els and make them vs
 
 -- ex 7: Run Length Encoding
 decode :: [(Char, Int)] -> String
 decode [] = ""                    -- base case for encoding
-decode (p:ps) = (replicate (snd p) (fst p)) ++ decode ps  -- replicates the head element
+-- decode (p:ps) = (replicate (snd p) (fst p)) ++ decode ps  -- replicates the head element
+decode ((char,count):tailOfList) = (replicate count char) ++ decode tailOfList
 
--- exsercise: write encoder for this 
+-- function to return the number of identical characters at the beginning of
+-- the string
+prefixCount :: String -> Int
+prefixCount []      = 0
+prefixCount (_:[])  = 1                     --(_:[]) is the pattern we are trying to match. this is a char array with one char followed by no others i.e. the single char string
+prefixCount (c1:c2:cs)
+ | c1 == c2        = 1 + prefixCount (c2:cs)
+ | otherwise       = 1                     -- i.e. we have a string of length 2
+
+
+-- exsercise: write encoder for this
+encode :: String -> [(Char, Int)]
+encode ""           = []
+ensode s            = ((head s, prefixCount s) : encode (drop (prefixCount s) s))
+
+
+-- List Comprehensions
+
+-- to remove all odd values in a list and return the result
+removeOdd :: [Int] -> [Int]
+removeOdd values = [val | val <- values, val `rem` 2 == 0]
+
+-- convert list of celcius temperatures to farenheit
+celciusToFarenheit :: [Float] -> [Float]
+celciusToFarenheit celc = [ (c * (9.0/5.0)) + 35.0 | c <- celc]
+
+-- square all of the even ints in list and discard all odds
+squareEvenDropOdd :: [Int] -> [Int]
+squareEvenDropOdd orig = [ x*x | x <- orig, x `rem` 2 == 0]
--- Text Exercises/Chapter5Exercises.hs
+++ Text Exercises/Chapter5Exercises.hs
@@ -1,3 +1,5 @@
+import Data.Char as Char
+
 -- 5.1 function returning the max two integers along with the number
 -- of times this Integer occurs in the pair
 maxOccurs :: Integer -> Integer -> (Integer, Integer)
@@ -48,26 +50,78 @@ xIntercept (0,b) = error "slope can not be 0"
 xIntercept (m,b) = (fromIntegral (-b))/(fromIntegral m)
 
 
--- 5.15
+-- 5.15 the expression [0, 0.1 .. 1] evaluates to [0.0,0.1,0.2,
+-- 0.30000000000000004,0.4000000000000001,0.5000000000000001,0.6000000000000001,
+-- 0.7000000000000001,0.8,0.9,1.0]. I thought it would evaluate to:
+-- [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]. Maybe this difference
+-- is a result of switching between evaluating Integer types and Float types?
 
--- 5.16
+-- 5.16 The list [2,3] contains two items, the Integers 2 and 3. The list
+-- [[2,3]] contains one item; namely the list [2,3]
 
--- 5.17
+-- 5.17 The result of evaluating [2 .. 2] in ghci is [2]
+-- The result of evaluating [2, 7 .. 4] is the list [2]. This is because the
+-- step is calculated as 7 - 2 = 5, but stepping from 7 would bring us to 12,
+-- which is already greater than the declared max element size of the last
+-- list element. Similarly, 7 is higher than this max, so it is also ignored,
+-- leaving the singleton, [2]
+-- Finally, evaluating [2, 2 .. 2] results in a list of 2's as large as can be
+-- held in memory. This is because the step is
+-- calculated as 2-2=0, so we will increment 2 by 0 and since this incrementation
+-- never leads to a value greater than 2, the list will keep growing accordingly.
 
--- 5.18
+-- 5.18 function to double all elements of a list and return result as a list
+doubleAll :: [Integer] -> [Integer]
+doubleAll listOfInts = [2*n | n <- listOfInts]
 
--- 5.19
+-- 5.19 function to take a String, i.e. list of Char, and capitalize small
+-- letter, returning the result as a String
+capitalize :: String -> String
+capitalize theString = [Char.toUpper x | x <- theString]
 
--- 5.20
+-- This version removes any non letters while capitalizing the letters
+capitalizeLetters :: String -> String
+capitalizeLetters theString = [Char.toUpper x | x <- theString, isLetter x]
 
--- 5.21
+-- 5.20 function to return the list of divisiors of a positive input Integer or
+-- an empty list if the input is something other than a String
+divisors :: Integer -> [Integer]
+divisors n = [ posibleDiv | posibleDiv <- [1 .. n], (n `mod ` posibleDiv) == 0]
 
--- 5.22
+isPrime :: Integer -> Bool
+isPrime n
+ | divisors n == [1,n]      = True
+ | otherwise                = False
 
--- 5.23
+-- 5.21 function  which picks out all occurrences of an integer n in a list,
+-- returning these integers as a list
+matches :: Integer -> [Integer] -> [Integer]
+matches n theList = [ i | i <- theList, i == n ]
 
--- 5.24
+-- function that uses matches to return True if the input Integer is an element
+-- of the list and False otherwise
+elemm :: Integer -> [Integer] -> Bool
+elemm n theList
+ | matches n theList == []  = False
+ | otherwise                = True
 
--- 5.26
+-- 5.22 TODO: check solution. function which takes a list of Strings and returns a single String ,
+-- which when printed, shows the strings on seperate lines
+--onSeperateLines :: [String] -> String
+--onSeperateLines st = [ str ++ ['\n'] | str <- st]
 
--- 5.28
+-- 5.23 function which takes a String and an Int and returns n copies of the
+-- string as a String
+duplicate :: String -> Integer -> String
+duplicate a n
+ | n == 0     = ""
+ | n < 0      = ""
+ | otherwise  = a ++ (duplicate a (n-1))
+
+-- 5.24 TODO: how would we make line length a parameter of this funciton?
+-- pushRight :: String -> String
+
+
+-- TODO: figure out 5.22 before doing this
+-- 5.26 a function which produces a table of fib numbers from 0 to input n
+-- fibTable :: Integer -> String
